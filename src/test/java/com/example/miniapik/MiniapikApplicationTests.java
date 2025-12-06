ab05f7a78f15750732e1ca948eeee4d214601ef1
Ayomide <oyetimehin31@gmail.com>
2025-12-06 15:01:20 +0100

    initial project setup

---
+++ src/test/java/com/example/miniapik/MiniapikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniapik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniapikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
